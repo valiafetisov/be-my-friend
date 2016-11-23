811149f6ddc563cfcac567bc8fe8a0129e49ce08
Valia Fetisov <valyalof@gmail.com>
2016-11-24 00:57:55 +0300

    rewrite TimelineReactiveDIV to function

--- imports/components/TimelineReactiveDIV.jsx
+++ imports/components/TimelineReactiveDIV.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 
-const TimelineReactiveDIV = React.createClass({
+const TimelineReactiveDIV = function(props) {
+  if (props.loading === true) return null
 
-  scale(time) {
+  const friends = props.friends
+  const periods = props.periods
+  const svgWidth = 100
+  const svgHeight = scale(props.now - props.min)
+  barWidth = svgWidth / friends.length
+  const style = {
+    width: '100%',
+    height: svgHeight + 'px'
+  }
+
+  function scale(time) {
     return time / 1000 / 60
-  },
+  }
 
-  renderPeriod(period, index, userID) {
+  function renderPeriod(period, index, userID) {
     if (userID !== period.userID) return
 
     const lastActive = (period.finished !== true || period.lastActive == null)
-      ? this.props.now
+      ? props.now
       : period.lastActive
-    const firstActiveScaled = this.scale(period.firstActive)
+    const firstActiveScaled = scale(period.firstActive)
     const style = {
       bottom: firstActiveScaled + 'px',
-      height: this.scale(lastActive) - firstActiveScaled + 'px'
+      height: scale(lastActive) - firstActiveScaled + 'px'
     }
 
     return <div
@@ -23,12 +34,12 @@ const TimelineReactiveDIV = React.createClass({
       className='period'
       style={style}
     />
-  },
+  }
 
-  renderFriend(friend, index) {
+  function renderFriend(friend, index) {
     const style = {
       height: '100%',
-      left: (this.barWidth * index) + '%'
+      left: (barWidth * index) + '%'
     }
 
     return <div
@@ -36,35 +47,17 @@ const TimelineReactiveDIV = React.createClass({
       className='friend'
       style={style}
     >
-      {this.props.periods.map((period, index) => this.renderPeriod(period, index, friend.userID))}
-    </div>
-  },
-
-  render() {
-    if (this.props.loading === true) return null
-
-    const friends = this.props.friends
-    const periods = this.props.periods
-    const svgWidth = 100
-    const svgHeight = this.scale(this.props.now - this.props.min)
-    this.barWidth = svgWidth / friends.length
-    const style = {
-      width: '100%',
-      height: svgHeight + 'px'
-    }
-
-    return <div className="TimelineDIV">
-      <div
-        className="svg"
-        style={style}
-      >
-        <div className="wrapper" style={{height: this.scale(this.props.now) + 'px'}}>
-          {friends.map(this.renderFriend)}
-        </div>
-      </div>
+      {props.periods.map((period, index) => renderPeriod(period, index, friend.userID))}
     </div>
   }
 
-})
+  return <div className="TimelineDIV">
+    <div className="svg" style={style}>
+      <div className="wrapper" style={{height: scale(props.now) + 'px'}}>
+        {friends.map(renderFriend)}
+      </div>
+    </div>
+  </div>
+}
 
 export default TimelineReactiveDIV
