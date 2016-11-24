6c5a9a7c7850d9b63e6a27351134bdb8552ef952
Valia Fetisov <valyalof@gmail.com>
2016-11-24 03:08:40 +0300

    TimelineReactiveDIV rewritten to smaller functional components

--- imports/components/TimelineReactiveDIV.jsx
+++ imports/components/TimelineReactiveDIV.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+function scale(time) {
+  return time / 1000 / 60
+}
+
 const TimelineReactiveDIV = function(props) {
   if (props.loading === true) return null
 
@@ -13,51 +17,50 @@ const TimelineReactiveDIV = function(props) {
     height: svgHeight + 'px'
   }
 
-  function scale(time) {
-    return time / 1000 / 60
-  }
-
-  function renderPeriod(period, index, userID) {
-    if (userID !== period.userID) return
-
-    const lastActive = (period.finished !== true || period.lastActive == null)
-      ? props.now
-      : period.lastActive
-    const firstActiveScaled = scale(period.firstActive)
-    const style = {
-      bottom: firstActiveScaled + 'px',
-      height: scale(lastActive) - firstActiveScaled + 'px'
-    }
-
-    return <div
-      key={'index_' + index}
-      className='period'
-      style={style}
-    />
-  }
-
-  function renderFriend(friend, index) {
-    const style = {
-      height: '100%',
-      left: (barWidth * index) + '%'
-    }
-
-    return <div
-      key={friend.userID}
-      className='friend'
-      style={style}
-    >
-      {props.periods.map((period, index) => renderPeriod(period, index, friend.userID))}
-    </div>
-  }
 
   return <div className="TimelineDIV">
     <div className="svg" style={style}>
       <div className="wrapper" style={{height: scale(props.now) + 'px'}}>
-        {friends.map(renderFriend)}
+        {friends.map((friend, index) => renderFriend(friend, index, periods, props.showInfo, props.now))}
       </div>
     </div>
   </div>
+}
+
+function renderFriend(friend, index, periods, showInfo, now) {
+  const style = {
+    height: '100%',
+    left: (barWidth * index) + '%'
+  }
+
+  return <div
+    key={friend.userID}
+    className='friend'
+    style={style}
+    onMouseOver={(e) => showInfo(e, friend)}
+    title={friend.fullName}
+  >
+    {periods.map((period, index) => renderPeriod(period, index, friend, now))}
+  </div>
+}
+
+function renderPeriod(period, index, friend, now) {
+  if (friend.userID !== period.userID) return
+
+  const lastActive = (period.finished !== true || period.lastActive == null)
+    ? now
+    : period.lastActive
+  const firstActiveScaled = scale(period.firstActive)
+  const style = {
+    bottom: firstActiveScaled + 'px',
+    height: scale(lastActive) - firstActiveScaled + 'px'
+  }
+
+  return <div
+    key={'index_' + index}
+    className='period'
+    style={style}
+  />
 }
 
 export default TimelineReactiveDIV
