ed69ab52a3961c6095c080526007b268ec077d4a
Valia Fetisov <valyalof@gmail.com>
2016-12-03 01:15:15 +0300

    add millisecondsPerTimelinePart to constants

--- imports/components/TimelinePartsInterator.jsx
+++ imports/components/TimelinePartsInterator.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import TimelinePartContainer from '/imports/containers/TimelinePartContainer'
 import TimelineSVG from '/imports/components/TimelineSVG'
+import Const from '/imports/libs/Const'
 
 const TimelinePartsInterator = React.createClass({
 
@@ -8,15 +9,14 @@ const TimelinePartsInterator = React.createClass({
     if (this.props.loading === true) return null
 
     // Calculate number of parts
-    const HOUR = 60 * 60 * 1000
     const diff = this.props.timelineStop - this.props.timelineStart
-    const hours = Math.floor(diff / HOUR)
+    const hours = Math.floor(diff / Const.millisecondsPerTimelinePart)
 
     // Create small timelines
     var blocks = []
     for (let i = hours; i >= 0; i--) {
-      const partStart = this.props.timelineStart + HOUR * i
-      let partStop = this.props.timelineStart + HOUR * (i + 1)
+      const partStart = this.props.timelineStart + Const.millisecondsPerTimelinePart * i
+      let partStop = this.props.timelineStart + Const.millisecondsPerTimelinePart * (i + 1)
       if (partStop > this.props.timelineStop) partStop = null
       const interval = (i === hours) ? 1000 : null
       const transmitFunction = (i === hours) ? this.props.transmitTimelineData : null
--- imports/libs/Const.js
+++ imports/libs/Const.js
@@ -1,6 +1,7 @@
 const Const = {
 
-  millisecondsPerPixel: 2 * 60 * 1000
+  millisecondsPerPixel: 2 * 60 * 1000,
+  millisecondsPerTimelinePart: 60 * 60 * 1000
 
 }
 
